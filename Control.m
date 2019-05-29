586f734cbc5853d84ec245679c17fd6746916491
S M NAHID MAHMUD <34007702+nahid04@users.noreply.github.com>
2019-05-29 13:32:19 -0500

    Update Control.m

--- Control.m
+++ Control.m
@@ -1,7 +1,19 @@
 clear all;
 clc;
-syms r_com x_com y_com z_com
+syms v_com r_com x_com y_com z_com r x y z yaw_com yaw r_dot_com
 
-r_com = transpose([x_com y_com z_com])
+r_com = transpose([x_com y_com z_com]);
+
+r = transpose ([x y z]);
+c1= 0.5;
+
+
+if c1 > 0
+v_com = c1*[r_com - r] + r_dot_com
+
+else v_com = r_dot_com
+    
+end
+
 
 
